57453d5f8260f4052ca4c721b9cf2a405bbdb9fd
Philipp A <flying-sheep@web.de>
2023-04-27 11:06:17 +0200

    SHould work

--- scripts/src/ecosystem_scripts/template_repo_registry.py
+++ scripts/src/ecosystem_scripts/template_repo_registry.py
@@ -1,13 +1,40 @@
+from __future__ import annotations
+
 import os
+from pathlib import Path
+from collections.abc import Iterable
+import sys
 
 from github import Github
+from yaml import safe_load, safe_dump
 
 
-def main():
-    g = Github(os.environ["GITHUB_TOKEN"])
-    results = g.search_code("filename:.cruft.json 'https://github.com/scverse/cookiecutter-scverse'")
-    for result in results:
-        print(result)
+def parse_repos(path: Path) -> set[str]:
+    if not path.is_file():
+        return set()
+    with path.open() as f:
+        return set(safe_load(f))
+
+
+def search_repos(github_token: str | None) -> set[str]:
+    g = Github(github_token)
+    files = g.search_code(
+        "filename:.cruft.json 'https://github.com/scverse/cookiecutter-scverse'"
+    )
+    repos = {file.repository for file in files}
+    return {r.url for r in repos}
+
+
+def main(args: Iterable[str] | None = None) -> None:
+    if args is None:
+        args = sys.argv[1:]
+    if len(args) != 1:
+        sys.exit("Usage: register-template-repos template-repos.yml")
+    path = Path(args[0])
+    repos = parse_repos(path) | search_repos(os.environ["GITHUB_TOKEN"])
+    if repos:
+        with path.open("w") as f:
+            safe_dump(sorted(repos), f)
 
 
 if __name__ == "__main__":
